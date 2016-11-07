7197ff2817c43be8c4b56121f7a5b92b5b8459f0
Abdelkader Boudih <terminale@gmail.com>
2016-11-07 21:00:34 +0000

    Jekyll 3.3 support

--- lib/jekyll/slim.rb
+++ lib/jekyll/slim.rb
@@ -1,7 +1,23 @@
-require 'ostruct'
 require 'sliq'
 require 'jekyll'
 require 'jekyll/slim/version'
-require 'jekyll/slim/extensions'
-require 'jekyll/slim/converter'
 require 'jekyll/slim/include_tag'
+
+module Jekyll
+  class SlimConverter < Converter
+    safe true
+    priority :low
+
+    def matches(ext)
+      ext =~ /slim/i
+    end
+
+    def output_ext(ext)
+      '.html'
+    end
+
+    def convert(content)
+      ::Sliq::Converter.new { content }.render
+    end
+  end
+end
--- lib/jekyll/slim/include_tag.rb
+++ lib/jekyll/slim/include_tag.rb
@@ -1,17 +1,11 @@
 module Jekyll
-  module Slim
-    class IncludeTag < Jekyll::Tags::IncludeTag
-      def source(file, context)
-        content = super
-        if file =~ /\.slim\Z/i
-          Converter.convert(context.registers[:site].config, content)
-        else
-          content
-        end
-      end
-    end
+  class SlimIncludeTag < Jekyll::Tags::IncludeTag
+
+  end
+  class SlimIncludeRelativeTag  < Jekyll::Tags::IncludeRelativeTag
+
   end
 end
 
-Liquid::Template.register_tag('slim', Jekyll::Slim::IncludeTag)
-Liquid::Template.register_tag('include', Jekyll::Slim::IncludeTag)
+Liquid::Template.register_tag('slim', Jekyll::SlimIncludeTag)
+Liquid::Template.register_tag('slim_relative', Jekyll::SlimIncludeRelativeTag)
